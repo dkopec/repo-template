c8a99460bff46469f9cf0f75dbfa08a86094a862
Dominik Kopec <kopec.dominik@gmail.com>
2021-05-21 13:33:45 -0400

    fix: release targets

--- prepare-release.sh
+++ prepare-release.sh
@@ -2,4 +2,6 @@
 
 # Packaging specific files that are needed for the to run the solution and put them in the dist folder so we don't specify file name.
 mkdir -p dist
-zip -qq -r "dist/Release_$1.zip" src/*
+
+# Change the * to specifc files best suited for your project.
+zip -qq -r dist/Release_$1.zip *
