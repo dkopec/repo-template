b83b50c390f3d6520171b8cee7ee409ad5100cc7
Dominik Kopec <kopec.dominik@gmail.com>
2021-05-21 13:26:09 -0400

    fix: as per linter

--- prepare-release.sh
+++ prepare-release.sh
@@ -2,4 +2,4 @@
 
 # Packaging specific files that are needed for the to run the solution and put them in the dist folder so we don't specify file name.
 mkdir -p dist
-zip -qq -r dist/Release_$1.zip src/*
+zip -qq -r "dist/Release_$1.zip" src/*
